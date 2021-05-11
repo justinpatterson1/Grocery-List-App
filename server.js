8a234751bfef9a81c153d75f91576864a7b8b3f9
Justin Patterson <justin.patterson17@yahoo.com>
2021-05-10 20:53:31 -0400

    Adding dot env file to secure connection

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
 const itemController = require("./controller/itemController.js");
 const app = express();
 
+require("dotenv").config({path:"config/keys.env"});
 app.use(express.json());
 app.use(cors({
     origin:`http://127.0.0.1:5500`
 }))
 app.use("/items",itemController)
-const PORT = 4000;
+const PORT = process.env.PORT;
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
-    mongoose.connect('mongodb+srv://admin:Password1@cluster0.t4mgf.mongodb.net/grocery-list?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+    mongoose.connect(process.env.MONGO_DB_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=>{
         console.log("Mongoose is alive");
     })
